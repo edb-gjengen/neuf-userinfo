0efcf25f0554319fbbded9207c04692d1e1832f5
Nikolai Kristiansen <nikolaik@gmail.com>
2011-10-10 21:40:26 +0200

    Password change and reset function with templates.
    Try reusing https://docs.djangoproject.com/en/dev/topics/auth/#module-django.contrib.auth.views to greatest extent.

--- main/forms.py
+++ main/forms.py
@@ -1,3 +1,8 @@
-from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth.forms import PasswordChangeForm, SetPasswordForm
 class LDAPPasswordChangeForm(PasswordChangeForm):
+    # TODO: Change password here
+    pass
+
+class LDAPSetPasswordForm(SetPasswordForm):
+    # TODO: Set password here
     pass
--- urls.py
+++ urls.py
@@ -1,17 +1,22 @@
 from django.conf.urls.defaults import *
 from django.contrib import admin
-from main.forms import LDAPPasswordChangeForm
+from main.forms import LDAPPasswordChangeForm, LDAPSetPasswordForm
 admin.autodiscover()
 
 urlpatterns = patterns('',
     (r'^admin/', include(admin.site.urls)),
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^accounts/$', 'main.views.profile'),
-    (r'^accounts/logout/$', 'django.contrib.auth.views.logout'),
     (r'^accounts/password/change$', 'django.contrib.auth.views.password_change',
         {'password_change_form': LDAPPasswordChangeForm}),
     (r'^accounts/password/change/done$', 'django.contrib.auth.views.password_change_done'),
+    (r'^accounts/password/reset$', 'django.contrib.auth.views.password_reset'),
+    (r'^accounts/password/reset/done$', 'django.contrib.auth.views.password_reset_done'),
+    (r'^accounts/password/reset/confirm$', 'django.contrib.auth.views.password_reset_confirm',
+        {'set_password_form': LDAPSetPasswordForm}),
+    (r'^accounts/password/reset/complete$', 'django.contrib.auth.views.password_reset_complete'),
     (r'^accounts/login/$', 'django.contrib.auth.views.login'),
+    (r'^accounts/logout/$', 'django.contrib.auth.views.logout'),
     (r'^accounts/(?P<username>)/', 'main.views.profile'),
     (r'^$', 'main.views.index'),
 )
