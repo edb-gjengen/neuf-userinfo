b8bfec68098bbf5e3db3e770bf854f6ad7ef88a3
Nikolai Kristiansen <nikolaik@gmail.com>
2011-12-25 22:57:41 +0100

    Style tooltips

--- static/js/status.js
+++ static/js/status.js
@@ -1,31 +1,30 @@
-
-/*$( document ).ajaxStart( function() {
-    $( '#spinner' ).show();
-}).ajaxStop( function() {
-    $( '#spinner' ).hide();
-});
-*/
-/* Get the service status of the user */
 $(function() {
-    var img_check = ' <img src="/static/check.png" alt="OK"';
-    var img_error = ' <img src="/static/exclamation_mark.png" alt="ERROR"';
+    /* Display the service status of the user */
+    var img_check = ' <img src="/static/check.png" alt="OK" />';
+    var img_error = ' <img src="/static/exclamation_mark.png" alt="ERROR" />';
     var image = '';
+    var title = '';
 
     $.getJSON("/userstatus/client/?format=json", function(data) {
         if(data.active) {
-            image = img_check + ' title="Du har en bruker til foreningsmaskinene. \nSiste vellykkede pålogging: ' + data.last_successful_auth + '" />';
+            image = img_check;
+            title = "Du har en bruker til foreningsmaskinene. \nSiste vellykkede pålogging: " + data.last_successful_auth;
         } else {
-            image = img_error + ' title="Ingen konto (mangler Kerberos principal)." />';
+            image = img_error;
+            title = "Ingen konto (mangler Kerberos principal).";
         }
-        $("#client_status").html(image);
+        $("#client_status").append(image).prop('title', title).qtip();
     });
     $.getJSON('/userstatus/wireless/?format=json', function(data) {
         if(data.active) {
-            image = img_check + ' title="Du har tilgang til trådløst nettverk. \nSiste vellykkede pålogging: ' + data.last_successful_auth + '" />';
+            image = img_check;
+            title = 'Du har tilgang til trådløst nettverk. \nSiste vellykkede pålogging: ' + data.last_successful_auth;
         } else {
-            image = img_error + ' title="Ingen trådløskonto (i Radius)." />';
+            image = img_error;
+            title = 'Ingen trådløskonto (i Radius).';
         }
-        $("#wireless_status").html(image);
+        $("#wireless_status").append(image).prop('title', title).qtip();
     });
-
+    /* Any element with a title attribute gets a pretty qtip */
+    $("[title]").qtip();
 });
